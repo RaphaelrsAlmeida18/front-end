789de036e382e26197dcb7c013fc1f3481d9e920
Raphael <raphaelrsalmeida@gmail.com>
2022-07-06 13:40:07 -0300

    mechendo no bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { ListaComponent } from './lista/lista.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'cadastro', component: CadastroComponent},
+  {path: 'lista', component: ListaComponent},
+  {path: '', redirectTo:'/cadastro', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
